99fb70ba9763d51d1351a28485d70fb17c329557
Tyler Harker <tyler@techfabric.co>
2018-07-26 10:05:57 -0700

    chore: created mapper utility

---
+++ CloudFabric.Library.Common/Utilities/MapperUtility.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudFabric.Library.Common.Utilities
+{
+    public static class MapperUtility
+    {
+        public static TOut Map<TIn, TOut>(TIn input)
+        {
+            return JsonConvert.DeserializeObject<TOut>(JsonConvert.SerializeObject(input));
+        }
+    }
+}
