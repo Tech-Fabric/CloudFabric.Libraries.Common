a959c7a72146e500e9879a56a1395e5330ae74ef
Tyler Harker <tyler@techfabric.co>
2018-12-03 15:46:47 -0700

    chore: global exception filter now handles custom exceptions, validation exceptions, as well as all server exceptions

--- CloudFabric.Library.Common/Filters/GlobalExceptionFilter.cs
+++ CloudFabric.Library.Common/Filters/GlobalExceptionFilter.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Net;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
+﻿using CloudFabric.Library.Common.Entities;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Net;
 
 namespace CloudFabric.Library.Common.Filters
 {
@@ -25,28 +28,60 @@ namespace CloudFabric.Library.Common.Filters
                 context.Exception,
                 context.Exception.Message);
 
+
+
             var errorDetails = new ErrorDetails()
             {
                 Instance = context.HttpContext.Request.Path,
                 Status = StatusCodes.Status500InternalServerError,
                 Detail = "Please refer to the errors property for additional details.",
-                Errors = {
-                            {
-                                "ServerError", new[] {"An unexpected error ocurred."}
-                            }
-                         },
+                Errors = { },
             };
+
+            if (context.Exception is BaseException)
+            {
+                var bException = (BaseException)context.Exception;
+                errorDetails.Errors.Add(context.Exception.GetType().Name, new[] { bException.Body() });
+                errorDetails.Status = (int)bException.Status();
+                context.Result = new ObjectResult(errorDetails)
+                {
+                    StatusCode = (int)bException.Status()
+                };
+                context.HttpContext.Response.StatusCode = (int)bException.Status();
+            }
+            else if (context?.ModelState?.IsValid == false)
+            {
+                foreach (var key in context.ModelState.Keys)
+                {
+                    ModelStateEntry val;
+                    context.ModelState.TryGetValue(key, out val);
+
+                    errorDetails.Errors.Add(key, val?.Errors?.Select(e => e.ErrorMessage).ToArray());
+                }
+                errorDetails.Status = (int)HttpStatusCode.BadRequest;
+                context.Result = new ObjectResult(errorDetails)
+                {
+                    StatusCode = StatusCodes.Status400BadRequest
+                };
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                errorDetails.Errors.Add("ServerError", new[] { "An unexpected error ocurred." });
+
+                context.Result = new ObjectResult(errorDetails)
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
 
             if (env.IsDevelopment())
             {
                 errorDetails.Exception = context.Exception;
             }
 
-            context.Result = new ObjectResult(errorDetails)
-            {
-                StatusCode = StatusCodes.Status500InternalServerError
-            };
-            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
 
             context.ExceptionHandled = true;
         }
