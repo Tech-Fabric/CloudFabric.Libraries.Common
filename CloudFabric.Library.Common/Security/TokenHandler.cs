27741c8080f1f5b78f9e883e17d2eef8942ee518
Preetham Reddy <preetham@techfabric.co>
2018-12-11 00:20:46 -0700

    Adding Token Handler to read bearer token

---
+++ CloudFabric.Library.Common/Security/TokenHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace CloudFabric.Library.Common.Security
+{
+    public static class TokenHandler
+    {
+        public static string GetBearerToken(string authHeader)
+        {
+            if (!string.IsNullOrEmpty(authHeader))
+            {
+                var token = authHeader.ToString().Split(' ');
+
+                if(token.Count() == 2)
+                {
+                    return token[1];
+                }                
+            }
+
+            return null;
+        }
+    }
+}
